abc35aacd65715b5ec8aa4836a4466e0d4d96a20
Alberto Begliardo <106992249+infobagubits@users.noreply.github.com>
2025-09-08 16:55:10 +0200

    chore: renaming feature

--- Plugin.php
+++ Plugin.php
@@ -18,12 +18,12 @@ class Plugin extends AbstractPlugin
     public function boot(): void
     {
         foreach (['php', 'php-blank', 'laravel', 'wordpress'] as $featureType) {
-            RegisterSiteFeature::make($featureType, 'vito-bagubits-filebackup')
+            RegisterSiteFeature::make($featureType, 'vito-filebackup')
                 ->label('Backup Files')
                 ->description('Backup files to AWS S3...')
                 ->register();
 
-            RegisterSiteFeatureAction::make($featureType, 'vito-bagubits-filebackup', 'backup-files')
+            RegisterSiteFeatureAction::make($featureType, 'vito-filebackup', 'backup-files')
                 ->label('Backup Files')
                 ->handler(Copy::class)
                 ->register();
